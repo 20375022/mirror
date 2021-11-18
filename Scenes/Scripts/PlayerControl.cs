3ebfe9daba33959dd9b6fb85bed6c09eaefae7bb
20375022 <71367607+20375022@users.noreply.github.com>
2021-11-18 14:57:12 +0900

    First-commit

---
+++ Scenes/Scripts/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerControl : NetworkBehaviour
+{
+    // 定期更新時に呼ばれる
+    void FixedUpdate()
+    {
+        // ローカルプレイヤーの時
+        if (isLocalPlayer)
+        {
+            var z = Input.GetAxis("Vertical");
+            Vector3 velocity = new Vector3(0, 0, z);
+            velocity = transform.TransformDirection(velocity);
+
+            var x = Input.GetAxis("Horizontal");
+            velocity = new Vector3(x, 0, 0);
+            velocity = transform.TransformDirection(velocity);
+
+            //// 操作
+            //float x = Input.GetAxis("Horizontal");
+            //float z = Input.GetAxis("Vertical");
+            CmdMoveSphere(x, z);
+        }
+    }
+
+    // 球の移動
+    [Command]
+    void CmdMoveSphere(float x, float z)
+    {
+        Vector3 v = new Vector3(x, 0, z) * 15f;
+        GetComponent<Rigidbody>().AddForce(v);
+    }
+}
