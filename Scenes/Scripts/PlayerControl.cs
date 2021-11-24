95fd206e761adbb9cb18012e36c30d4c78a19d29
20375022 <71367607+20375022@users.noreply.github.com>
2021-11-24 10:30:38 +0900

    controll

--- Scenes/Scripts/PlayerControl.cs
+++ Scenes/Scripts/PlayerControl.cs
@@ -16,19 +16,19 @@ public class PlayerControl : NetworkBehaviour
     // 球の移動
     void CmdMoveSphere()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(0f, 0f, 0.1f);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(0f, 0f, -0.1f);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-0.1f, 0f, 0f);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(0.1f, 0f, 0f);
         }
