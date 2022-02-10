ae344373c25ee3342e197f8e373903ea24f40168
20375022 <71367607+20375022@users.noreply.github.com>
2022-02-10 12:13:46 +0900

    a

--- Scenes/Scripts/CharactorManager.cs
+++ Scenes/Scripts/CharactorManager.cs
@@ -33,8 +33,10 @@ public class CharactorManager : NetworkBehaviour
     public GameObject networkPanel;
     public GameObject selectPanel;
     public static PlayerType playerType;
-    public static KillerType killerType;
-    public static SurvivorType survivorType;
+    int killerType;
+    int survivorType;
+    public GameObject SelectKillerModel;
+    public GameObject SelectSurvivorModel;
     public GameObject Enteradress;
 
     // Start is called before the first frame update
@@ -43,8 +45,10 @@ public class CharactorManager : NetworkBehaviour
         networkPanel.SetActive(true);
         selectPanel.SetActive(false);
         playerType = PlayerType.none;
-        killerType = KillerType.killer;
-        survivorType = SurvivorType.runner;
+        killerType = (int)KillerType.killer;
+        survivorType = (int)SurvivorType.runner;
+        SelectKillerModel.transform.GetChild(killerType).gameObject.SetActive(true);
+        SelectSurvivorModel.transform.GetChild(survivorType).gameObject.SetActive(true);
     }
 
     //ボタンを有効化
@@ -64,6 +68,7 @@ public class CharactorManager : NetworkBehaviour
         if (playerType == PlayerType.none)
         {
             selectPanel.SetActive(true);
+            networkPanel.SetActive(false);
         }
         else
         {
@@ -75,6 +80,8 @@ public class CharactorManager : NetworkBehaviour
     void DisabledBtn() 
     {
         selectPanel.SetActive(false);
+        Destroy(SelectKillerModel);
+        Destroy(SelectSurvivorModel);
         Cmdplytypechg(playerType);
     }
 
@@ -82,35 +89,116 @@ public class CharactorManager : NetworkBehaviour
     public void OnKillerPlus()
     {
         Debug.Log("キラー +");
+        SelectKillerModel.transform.GetChild(killerType).gameObject.SetActive(false);
+        if (killerType == (int)KillerType.piero)
+        {
+            killerType = (int)KillerType.killer;
+        }
+        else
+        {
+            killerType++;
+        }
+        SelectKillerModel.transform.GetChild(killerType).gameObject.SetActive(true);
     }
     //Killerのモデル選択(右ボタン)
     public void OnKillerMinus()
     {
         Debug.Log("キラー -");
+        SelectKillerModel.transform.GetChild(killerType).gameObject.SetActive(false);
+        if (killerType == (int)KillerType.killer)
+        {
+            killerType = (int)KillerType.piero;
+        }
+        else
+        {
+            killerType--;
+        }
+        SelectKillerModel.transform.GetChild(killerType).gameObject.SetActive(true);
     }
 
     //Survivorのモデル選択(左ボタン)
     public void OnSurvivorPlus()
     {
         Debug.Log("サバイバー +");
-
+        SelectSurvivorModel.transform.GetChild(survivorType).gameObject.SetActive(false);
+        if (survivorType == (int)SurvivorType.sutai)
+        {
+            survivorType = (int)SurvivorType.runner;
+        }
+        else
+        {
+            survivorType++;
+        }
+        SelectSurvivorModel.transform.GetChild(survivorType).gameObject.SetActive(true);
     }
     //Survivorのモデル選択(右ボタン)
     public void OnSurvivorMinus()
     {
         Debug.Log("サバイバー -");
+        SelectSurvivorModel.transform.GetChild(survivorType).gameObject.SetActive(false);
+        if (survivorType == (int)SurvivorType.runner)
+        {
+            survivorType = (int)SurvivorType.sutai;
+        }
+        else
+        {
+            survivorType--;
+        }
+        SelectSurvivorModel.transform.GetChild(survivorType).gameObject.SetActive(true);
     }
 
 
-    /*
-        //テクニカルタイプ選択
-        public void SelectTechniquetype()
+
+    //テクニカルタイプ選択
+    public void SelectedModel()
+    {
+        Debug.Log("Selected");
+        switch (killerType)
         {
-            Debug.Log("technique");
-            playerType = PlayerType.tec;
-    //        Cmdplytypechg(playerType);
-            DisabledBtn();
-        }*/
+            case (int)KillerType.killer:
+                switch (survivorType)
+                {
+                    case(int)SurvivorType.runner:
+                        Debug.Log("killrun");
+                        playerType = PlayerType.killrun;
+                        DisabledBtn();
+                        break;
+
+                    case (int)SurvivorType.sutai:
+                        Debug.Log("killsu");
+                        playerType = PlayerType.killsu;
+                        DisabledBtn();
+                        break;
+
+//                    case (int)SurvivorType:
+//                        break;
+                }
+                break;
+
+            case (int)KillerType.piero:
+                switch (survivorType)
+                {
+                    case(int)SurvivorType.runner:
+                        Debug.Log("pierun");
+                        playerType = PlayerType.pierun;
+                        DisabledBtn();
+                        break;
+
+                    case (int)SurvivorType.sutai:
+                        Debug.Log("piesu");
+                        playerType = PlayerType.piesu;
+                        DisabledBtn();
+                        break;
+
+//                    case (int)SurvivorType:
+//                        break;
+                }
+                break;
+
+//            case (int)KillerType:
+//                break;
+        }
+    }
 
     [Command]
     void Cmdplytypechg(PlayerType pt)
---
+++ Scenes/Scripts/FollowDestroy.cs
@@ -0,0 +1,17 @@
+﻿/*  -----------------------------------------------------   */
+/*         消えた時に何かのオブジェクトを道連れにする       */
+/*  -----------------------------------------------------   */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowDestroy : MonoBehaviour
+{
+    public GameObject followDestroyObject;
+
+
+    void OnDestroy()
+    {
+        Destroy(followDestroyObject);
+    }
+}
