5c737c21a8fbd48b678c7f1c94ed3eb77902ae1a
20375034 <em20375034@ga.tera-house.ac.jp>
2022-02-14 17:04:06 +0900

    a

--- Scenes/Scripts/TransitionLight.cs
+++ Scenes/Scripts/TransitionLight.cs
@@ -1,37 +1,20 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-public class TransitionDark : MonoBehaviour
+public class TransitionLight : MonoBehaviour
 {
     [SerializeField]
     private Material _transitionIn;
-    Title Startmanager;
-    bool Indark = false;
-    public GameObject Titlemanager;
-    public Material mat;
 
     void Start()
     {
-        mat.SetFloat("_Alpha", 0);
-        Startmanager = Titlemanager.GetComponent<Title>();
+        StartCoroutine(BeginTransition());
     }
 
-    void Update() {
-//        Debug.Log("flg" + Startmanager.Darkstart);
-        if (Startmanager.Darkstart == true)
-        {
-            if (Indark == false)
-            {
-                Indark = true;
-                StartCoroutine(BeginTransition());
-            }
-        }
-        else {
-            mat.SetFloat("_Alpha", 0);
-        }
+    void Update()
+    {
     }
 
     IEnumerator BeginTransition()
@@ -46,8 +29,8 @@ public class TransitionDark : MonoBehaviour
     /// <returns></returns>
     IEnumerator Animate(Material material, float time)
     {
-        float current = 0;
         GetComponent<Image>().material = material;
+        float current = 0;
         while (current < time)
         {
             material.SetFloat("_Alpha", current / time);
@@ -55,7 +38,6 @@ public class TransitionDark : MonoBehaviour
             current += Time.deltaTime;
         }
         material.SetFloat("_Alpha", 1);
-        yield return new WaitForSeconds(1.2f); 
-        Startmanager.BeginGame();
     }
 }
+
