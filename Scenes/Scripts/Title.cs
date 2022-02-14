59d39ef6936f21689cf891e96d042a0bb81213fc
20375034 <em20375034@ga.tera-house.ac.jp>
2022-02-14 15:58:03 +0900

    a

--- Scenes/Scripts/Title.cs
+++ Scenes/Scripts/Title.cs
@@ -5,10 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
-    public void OnClickStart()
-    {
-        SceneManager.LoadScene("game");
+    public bool Darkstart;
+//    TransitionDark Dark = GameObject.Find("brack").GetComponent<TransitionDark>();
+
+    void Start() {
+        Darkstart = false;
+//        Debug.Log("TStart");
     }
 
+    void Update()
+    {
+        Debug.Log("push" + Darkstart);
+//        Debug.Log("TUpdate");
+    }
+
+    public void OnClickStart()
+    {
+        Darkstart = true;
+    }
+
+    public void BeginGame() {
+        SceneManager.LoadScene("game");
+    }
 
 }
---
+++ Scenes/Scripts/TransitionDark.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class TransitionDark : MonoBehaviour
+{
+    [SerializeField]
+    private Material _transitionIn;
+    Title Startmanager;
+    bool Indark = false;
+    public GameObject Titlemanager;
+    float current;
+
+    void Start()
+    {
+//        Debug.Log("DStart");
+        current = 0;
+        Startmanager = Titlemanager.GetComponent<Title>() ;
+    }
+
+    void Update() {
+//        Debug.Log("DUpdate");
+        Debug.Log("flg" + Startmanager.Darkstart);
+        if (Startmanager.Darkstart == true && Indark == false)
+        {
+            Indark = true;
+            StartCoroutine(BeginTransition());
+        }
+    }
+
+    IEnumerator BeginTransition()
+    {
+        yield return Animate(_transitionIn, 1);
+    }
+
+    /// <summary>
+    /// time秒かけてトランジションを行う
+    /// </summary>
+    /// <param name="time"></param>
+    /// <returns></returns>
+    IEnumerator Animate(Material material, float time)
+    {
+//        GetComponent<Image>().material = material;
+        while (current < time)
+        {
+            material.SetFloat("_Alpha", current / time);
+            yield return new WaitForEndOfFrame();
+            current += Time.deltaTime;
+        }
+        material.SetFloat("_Alpha", 1);
+        Startmanager.BeginGame();
+    }
+}
